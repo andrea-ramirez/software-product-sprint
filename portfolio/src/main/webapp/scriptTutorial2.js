dd851c9722608f6022cc9fa1a4053b670292c421
Andrea Ramirez <a00825062@itesm.mx>
2021-03-15 22:34:23 +0000

    Return a list of of messages from servlet

--- portfolio/src/main/webapp/scriptTutorial2.js
+++ portfolio/src/main/webapp/scriptTutorial2.js
@@ -1,8 +1,27 @@
-/** Fetches the hardcoded message from the server and adds it to the page. */
-async function showHardCodedString() {
-  const responseFromServer = await fetch('/hello');
-  const textFromResponse = await responseFromServer.text();
+/** Fetches the JSON string from the server and adds it to the page. */
+async function showRandomMessage() {
+  const responseFromServer = await fetch('/randomMessage');
+  const randomMessage = await responseFromServer.json();
+
+  //console.log(randomMessage);
 
   const stringContainer = document.getElementById('string-container');
-  stringContainer.innerText = textFromResponse;
+  const message = randomMessage[Math.floor(Math.random() * randomMessage.length)];
+  stringContainer.innerText = message;
+
+}
+
+/**
+ * Adds a random fun fact to the page.
+ */
+function addRandomFunFact() {
+  const funFacts =
+      ['I have three nationalities', 'I have never eaten an orange', 'An UX Designer was the first person that encouraged me to study CS', 'I am a Gryffindor! :)'];
+
+  // Pick a random fun fact from the array.
+  const fact = funFacts[Math.floor(Math.random() * funFacts.length)];
+
+  // Add it to the page.
+  const funFactsContainer = document.getElementById('funFacts-container');
+  funFactsContainer.innerText = fact;
 }
